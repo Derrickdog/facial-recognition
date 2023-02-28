47d13c9a67e4587b8cf1c29ab769417fac5457de
Derrick <derrickryan10@gmail.com>
2023-02-28 14:38:41 -0500

    Add SignIn and Register

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import { Component } from 'react';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 
 class App extends Component {
   constructor() {
@@ -12,6 +14,8 @@ class App extends Component {
     this.state = {
       input: '',
       imageUrl: '',
+      route: 'signin',
+      isSignedIn: false,
     }
   }
 
@@ -23,15 +27,37 @@ class App extends Component {
     this.setState({imageUrl: this.state.input})
   }
 
+  onRouteChange = (route) => {
+    if(route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if(route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route})
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route } = this.state;
     return (
       <div>
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
         <ParticlesBg type="circle" bg={true} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        {
+          route === 'home'
+          ?
+          <div>
+          <Logo />
+          <Rank />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+          <FaceRecognition imageUrl={imageUrl}/>
+        </div>
+          
+          : (
+            route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange}/>
+            : <Register onRouteChange={this.onRouteChange} />
+          )
+        }
       </div>
     );
   }
