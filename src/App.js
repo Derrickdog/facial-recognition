1bd08a5d21d53c04e21d92113dbc445bd1c6a33b
Derrick <derrickryan10@gmail.com>
2023-02-28 11:21:59 -0500

    Add listeners

--- src/App.js
+++ src/App.js
@@ -4,18 +4,36 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-      <ParticlesBg type="circle" bg={true} />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit} />
+        {/* <FaceRecognition /> */}
+        <ParticlesBg type="circle" bg={true} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f2'>
@@ -8,8 +8,8 @@ const ImageLinkForm = () => {
       </p>
       <div className='center'>
         <div className="center pa4 br3 shadow-5" style={{width: '700px', background: 'linear-gradient(to right, #0099cc, #33cccc)'}}>
-          <input className='f4 pa2 w-70 center' type='text' />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-blue'>Detect</button>
+          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-blue' onClick={onButtonSubmit}>Detect</button>
         </div>
       </div>
     </div>
